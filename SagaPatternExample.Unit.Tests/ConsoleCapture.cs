c9d0548052cf2ba577ac285121c2cc9fafd7e448
sliusarenko.denys@gmail.com <sliusarenko.denys@gmail.com>
2026-02-11 13:06:35 +0200

    fix

--- SagaPatternExample.Unit.Tests/ConsoleCapture.cs
+++ SagaPatternExample.Unit.Tests/ConsoleCapture.cs
@@ -1,21 +1,19 @@
-﻿namespace SagaPatternExample.Unit.Tests
+﻿namespace SagaPatternExample.Unit.Tests;
+
+public class ConsoleCapture : IDisposable
 {
-    public class ConsoleCapture : IDisposable
+    private readonly TextWriter _originalOut;
+    public StringWriter Output { get; }
+
+    public ConsoleCapture()
     {
-        private readonly TextWriter _originalOut;
-        public StringWriter Output { get; }
-
-        public ConsoleCapture()
-        {
-            _originalOut = Console.Out;
-            Output = new StringWriter();
-            Console.SetOut(Output);
-        }
-
-        public void Dispose()
-        {
-            Console.SetOut(_originalOut);
-        }
+        _originalOut = Console.Out;
+        Output = new StringWriter();
+        Console.SetOut(Output);
     }
 
+    public void Dispose()
+    {
+        Console.SetOut(_originalOut);
+    }
 }
